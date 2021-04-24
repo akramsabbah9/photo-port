0d1a5796933a79d0b7bb0a14049ff2e09e2039dc
Akram Sabbah <akramsabbah9@gmail.com>
2021-04-23 22:01:42 -0700

    completed modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Modal({currentPhoto}) {
+function Modal({ currentPhoto, onClose }) {
     const { name, category, description, index } = currentPhoto;
     return (
         <div className="modalBackdrop">
@@ -8,10 +8,10 @@ function Modal({currentPhoto}) {
                 <h3 className="modalTitle">{name}</h3>
                 <img
                     src={require(`../../assets/large/${category}/${index}.jpg`).default}
-                    alt="current category"
+                    alt={category}
                 />
                 <p>{description}</p>
-                <button type="button">Close this modal</button>
+                <button type="button" onClick={onClose}>Close this modal</button>
             </div>
         </div>
     );
